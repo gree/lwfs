a4bc1d05499ce549ce3249045651dc539493535f
Koji Nakamaru <koji.nakamaru@gree.net>
2013-02-14 17:23:26 +0900

    adjusted stop.js to kill watch*.rb adequately.

--- build/win/stop.js
+++ build/win/stop.js
@@ -3,10 +3,10 @@ var objs = svc.InstancesOf("Win32_Process");
 for (var e = new Enumerator(objs); !e.atEnd(); e.moveNext()) {
     var item = e.item();
     if (item.CommandLine) {
-        if (item.CommandLine.match(/(java|ruby)(.exe)?.*lib\/watch-win.rb/)) {
+        if (item.CommandLine.match(/(java|ruby)(.exe)?.*lib\/watch.*\.rb/)) {
             item.Terminate();
         }
-        if (item.CommandLine.match(/(java|ruby)(.exe)?.*lwfs.rb/)) {
+        if (item.CommandLine.match(/(java|ruby)(.exe)?.*lwfs\.rb/)) {
             item.Terminate();
         }
     }
