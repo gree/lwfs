77040009121672e7c017536e3e9091b6921404f2
Koji Nakamaru <koji.nakamaru@gree.net>
2013-05-31 21:41:16 +0900

    removed auto-reloader.js and enhanced loading.js to unity status check and reload.

--- tmpl/js/loading.js
+++ tmpl/js/loading.js
@@ -1,5 +1,6 @@
 (function() {
     INTERVAL = 500;
+    UPDATE_TIME = '0';
     // cf. http://d.hatena.ne.jp/amachang/20061201/1164986067
     var currentScript
         = (function (e) {
@@ -13,33 +14,44 @@
             var attr = currentScript.attributes[i];
             if (attr.name === "interval") {
                 INTERVAL = ~~Math.round(parseFloat(attr.value) * 1000);
+            } else if (attr.name === "update_time") {
+                UPDATE_TIME = attr.value;
             }
         }
     }
-    window.addEventListener(
-        (window.onpageshow !== undefined) ? 'pageshow' : 'load',
-        function() {
-            var elm = document.getElementById('loading');
-            if (elm != null) {
-                var checkStatus = function() {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', location.href.replace(/[^\/]+$/, '') + '.loading?t=' + Date.now(), true);
-                    xhr.onreadystatechange = function() {
-                        if(xhr.readyState === 4){
-                            if (xhr.status === 200) {
-                                var res = JSON.parse(xhr.responseText);
-                                if (elm != null) {
-                                    elm.style.visibility = (res.is_in_conversion) ? 'visible' : 'hidden';
+    var isFile = /^file:/.test(window.location.href);
+    if (! isFile) {
+        window.addEventListener(
+            (window.onpageshow !== undefined) ? 'pageshow' : 'load',
+            function() {
+                var elm = document.getElementById('loading_icon');
+                if (elm != null) {
+                    var checkStatus = function() {
+                        var xhr = new XMLHttpRequest();
+                        xhr.open('GET', window.location.href.replace(/[^\/]+$/, '') + '.loading', true);
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState === 4) {
+                                if (xhr.status === 200) {
+                                    var res = JSON.parse(xhr.responseText);
+                                    if (res.update_time !== undefined && res.update_time != UPDATE_TIME) {
+                                        setTimeout(
+                                            function() {
+                                                window.location.reload();
+                                            },
+                                            100);
+                                    } else if (elm != null) {
+                                        elm.style.visibility = (res.is_in_conversion) ? 'visible' : 'hidden';
+                                    }
                                 }
                             }
-                        }
+                        };
+                        xhr.setRequestHeader('If-Modified-Since', 'Thu, 01 Jun 1970 00:00:00 GMT');
+                        xhr.send('');
+                        setTimeout(checkStatus, INTERVAL);
                     };
-                    xhr.send('');
                     setTimeout(checkStatus, INTERVAL);
-                };
-                // use large duration initially for avoiding jittered loading display on reload.
-                setTimeout(checkStatus, 2000);
-            }
-        },
-        false);
+                }
+            },
+            false);
+    }
 }).call(this);
