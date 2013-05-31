c543ab1bdaf30b82a2926e4314ffa85fe4fbf6b2
Koji Nakamaru <koji.nakamaru@gree.net>
2013-05-31 21:30:59 +0900

    fixed remote/lwfs-cleanup-daily.sh to actually work.

--- remote/lwfs-cleanup-daily.sh
+++ remote/lwfs-cleanup-daily.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 cd /home/SERVERS
-find lwfs -atime +7 | grep '^lwfs/[0-9abcdef\-]+$'
+
+dt=15552000 # 60 * 60 * 24 * 30 * 6
+d0=`date +%s`
+d0=`expr $d0 - $dt`
+
+for i in lwfs/[0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef]*
+do
+    d=`find $i -print0 | xargs -0 stat -c %y | sort -n -r | head -1`
+    d=`date +%s -d "$d"`
+    if [ $d -lt $d0 ]
+    then
+	echo $i
+    fi
+done | xargs rm -rf
