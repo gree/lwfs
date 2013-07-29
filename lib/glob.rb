211526ad4f3ee4f6b0a695b87b90138f03f9525b
Koji Nakamaru <koji.nakamaru@gree.net>
2013-07-29 19:26:21 +0900

    modified glob.rb to support ignored_pattern as an argument.

--- lib/glob.rb
+++ lib/glob.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-def glob(filter)
+def glob(filter, ignored_pattern = nil)
+  ignored_pattern = /(^|\/)[,#]/ if ignored_pattern.nil?
   entries = []
   if filter.kind_of?(Array)
     filter = filter.map{|e| e.encode(Encoding::UTF_8)}
@@ -8,7 +9,7 @@ def glob(filter)
     filter = filter.encode(Encoding::UTF_8)
   end
   Dir.glob(filter).each do |e|
-    entries.push(e) unless e =~ /(^|\/)[#,]/
+    entries.push(e) unless e =~ ignored_pattern
   end
   entries.sort
 end
