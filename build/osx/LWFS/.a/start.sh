f61b50ea6db5332035a28b86ca0c9bf38dcf8213
Koji Nakamaru <koji.nakamaru@gree.net>
2020-05-14 12:35:38 +0900

    updated osx ruby runtime with 2.7.1/64bit for catalina.

--- build/osx/LWFS/.a/start.sh
+++ build/osx/LWFS/.a/start.sh
@@ -3,7 +3,7 @@
 BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # ruby environment
-RBH="$BASE/ruby23"
+RBH=$(echo "$BASE"/ruby*)
 export PATH="$RBH/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 if [ -n "$DYLD_LIBRARY_PATH" ]; then
   export DYLD_LIBRARY_PATH="$RBH/lib:$DYLD_LIBRARY_PATH"
@@ -11,9 +11,9 @@ else
   export DYLD_LIBRARY_PATH="$RBH/lib"
 fi
 export RUBYOPT='-Ku'
-export RUBYLIB="$RBH/lib/ruby/2.3.0:$RBH/lib/ruby/2.3.0/i386-darwin10.8.0"
-export GEM_HOME="$RBH/lib/ruby/gems/2.3.0"
-export GEM_PATH="$RBH/lib/ruby/gems/2.3.0:$RBH/lib/ruby/2.3.0"
+export RUBYLIB="$RBH/lib/ruby/2.7.0:$RBH/lib/ruby/2.7.0/x86_64-darwin15"
+export GEM_HOME="$RBH/lib/ruby/gems/2.7.0"
+export GEM_PATH="$RBH/lib/ruby/gems/2.7.0:$RBH/lib/ruby/2.7.0"
 unset IRBRC
 unset MAGLEV_HOME
 unset RBXOPT
